f06897b4d0582bf37a95d4a1441bd07e7073f368
Bhumit Mittal <bhumit9416@gmail.com>
2025-06-29 04:09:12 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'DRDO Project',
+  description: 'DRDO Project',
+  generator: 'DRDO Project',
 }
 
 export default function RootLayout({
